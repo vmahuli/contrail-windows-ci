d73c842c55a3c98e0a740e2ba055832f0b4e90ad
Mateusz Jabłoński <mateusz.jablonski@codilime.com>
2017-07-17 16:02:14 +0200

    JW-868: Vtest scenarios test (#20)

--- CIScripts/TestIntegration.ps1
+++ CIScripts/TestIntegration.ps1
@@ -51,7 +51,7 @@ $TestConfiguration = [TestConfiguration] @{
 # TODO: JW-838: Add parameters after tests implementation
 Test-ExtensionLongLeak -Session $Sessions[0] -TestDurationHours $Env:LEAK_TEST_DURATION -TestConfiguration $TestConfiguration
 Test-MultiEnableDisableExtension
-Test-VTestScenarios
+Test-VTestScenarios -Session $Sessions[0] -TestConfiguration $TestConfiguration
 Test-TCPCommunication
 Test-ICMPOverMPLSOverGRE
 Test-TCPOverMPLSOverGRE
--- CIScripts/Tests/VTestScenariosTest.ps1
+++ CIScripts/Tests/VTestScenariosTest.ps1
@@ -1,3 +1,27 @@
 function Test-VTestScenarios {
-    # TODO: JW-838: Implement
+    Param ([Parameter(Mandatory = $true)] [System.Management.Automation.Runspaces.PSSession] $Session,
+           [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
+
+    Write-Host "Running vtest scenarios"
+
+    Initialize-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
+
+    $VMSwitchName = $TestConfiguration.VMSwitchName
+    $Res = Invoke-Command -Session $Session -ScriptBlock {
+        Push-Location C:\Artifacts\
+
+        vtest\all_tests_run.ps1 -VMSwitchName $Using:VMSwitchName -TestsFolder vtest\tests | Write-Host
+        $Res = $LASTEXITCODE
+
+        Pop-Location
+        return $Res
+    }
+
+    if ($Res -ne 0) {
+        throw "VTest scenarios test failed!"
+    }
+
+    Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
+
+    Write-Host "Success!"
 }
