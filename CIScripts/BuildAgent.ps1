eccd2eaa0e4653f43de1a10e51ea8af49b816cbd
Mateusz Jabłoński <mateusz.jablonski@codilime.com>
2017-08-03 15:13:05 +0200

    JW-881: Multi-branch build (#31)

--- CIScripts/BuildAgent.ps1
+++ CIScripts/BuildAgent.ps1
@@ -13,12 +13,47 @@ Write-Host "Sourcing VS environment variables"
 Invoke-BatchFile "$Env:VS_SETUP_ENV_SCRIPT_PATH"
 
 Write-Host "Cloning repositories"
-git clone -b $Env:TOOLS_BRANCH $Env:TOOLS_REPO_URL tools/build/
-git clone -b $Env:VROUTER_BRANCH $Env:VROUTER_REPO_URL vrouter/
-git clone -b $Env:WINDOWSSTUBS_BRANCH $Env:WINDOWSSTUBS_REPO_URL windows/
-git clone -b $Env:SANDESH_BRANCH $Env:SANDESH_REPO_URL tools/sandesh/
-git clone -b $Env:CONTROLLER_BRANCH $Env:CONTROLLER_REPO_URL controller/
-git clone -b $Env:GENERATEDS_BRANCH $Env:GENERATEDS_REPO_URL tools/generateDS/
+class Repo {
+    [string] $Url;
+    [string] $Branch;
+    [string] $DefaultBranch;
+    [string] $Dir;
+
+    Repo ([string] $Url, [string] $Branch, [string] $Dir, [string] $DefaultBranch) {
+        $this.Url = $Url
+        $this.Branch = $Branch
+        $this.Dir = $Dir
+        $this.DefaultBranch = $DefaultBranch
+    }
+}
+
+$Repos = @(
+    [Repo]::new($Env:TOOLS_REPO_URL, $Env:TOOLS_BRANCH, "tools/build/", "windows"),
+    [Repo]::new($Env:SANDESH_REPO_URL, $Env:SANDESH_BRANCH, "tools/sandesh/", "windows"),
+    [Repo]::new($Env:GENERATEDS_REPO_URL, $Env:GENERATEDS_BRANCH, "tools/generateDS/", "windows"),
+    [Repo]::new($Env:VROUTER_REPO_URL, $Env:VROUTER_BRANCH, "vrouter/", "windows"),
+    [Repo]::new($Env:WINDOWSSTUBS_REPO_URL, $Env:WINDOWSSTUBS_BRANCH, "windows/", "windows"),
+    [Repo]::new($Env:CONTROLLER_REPO_URL, $Env:CONTROLLER_BRANCH, "controller/", "windows3.1")
+)
+
+$CustomBranches = @($Repos.Where({ $_.Branch -ne $_.DefaultBranch }) | Select-Object -ExpandProperty Branch -Unique)
+$Repos.ForEach({
+    # If there is only one unique custom branch provided, at first try to use it for all repos.
+    # Otherwise, use branch specific for this repo.
+    $CustomMultiBranch = $(if ($CustomBranches.Count -eq 1) { $CustomBranches[0] } else { $_.Branch })
+
+    Write-Host $("Cloning " +  $_.Url + " from branch: " + $CustomMultiBranch)
+    git clone -b $CustomMultiBranch $_.Url $_.Dir
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host $("Cloning " +  $_.Url + " from branch: " + $_.Branch)
+        git clone -b $_.Branch $_.Url $_.Dir
+
+        if ($LASTEXITCODE -ne 0) {
+            throw "Cloning from " + $_.Url + " failed"
+        }
+    }
+})
 
 Write-Host "Copying third-party dependencies"
 New-Item -ItemType Directory ./third_party
