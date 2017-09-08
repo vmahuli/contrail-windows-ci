b6cfa292ccb1488401c6a41fba627b7c6d6f21a4
Mateusz Jabłoński <mateusz.jablonski@codilime.com>
2017-09-08 17:50:46 +0200

    JW-1120: Fix abandoned mutex problem (#59)
    * JW-1120: Fix abandoned mutex problem
    
    * Requested changes

--- CIScripts/VMUtils.ps1
+++ CIScripts/VMUtils.ps1
@@ -21,7 +21,17 @@ function Initialize-VIServer {
     # Global named mutex needed here because all PowerCLI commnads are running in global context and modify common configuration files.
     # Without this mutex, PowerCLI commands may throw an exception in case of configuration file being blocked by another job.
     $Mutex = [System.Threading.Mutex]::new($false, "WinContrailCIPowerCLIMutex")
-    [void] $Mutex.WaitOne()
+    $MaxTimeout = 1000 * 60 * 10 # 10 min
+
+    try {
+        if (!$Mutex.WaitOne($MaxTimeout)) {
+            throw "Timeout while waiting for mutex"
+        }
+    }
+    catch [System.Threading.AbandonedMutexException] {
+        [void] $Mutex.Close()
+        throw "Global mutex has been abandoned. Try restarting the job."
+    }
 
     try {
         $Res = Get-Command -Name Connect-VIServer -CommandType Cmdlet -ErrorAction SilentlyContinue
@@ -36,6 +46,7 @@ function Initialize-VIServer {
     }
     finally {
         [void] $Mutex.ReleaseMutex()
+        [void] $Mutex.Close()
     }
 
     Pop-Location
