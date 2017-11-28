4dfcc66993ae3f192e76d295fafb13f858f7efbb
Paweł Marchewka <pawel.marchewka@codilime.com>
2017-11-28 16:31:02 +0100

    JW-442 : Added Windows - Linux UDP test (#122)

--- CIScripts/Test/Tests/WindowsLinuxIntegrationTests.ps1
+++ CIScripts/Test/Tests/WindowsLinuxIntegrationTests.ps1
@@ -39,12 +39,58 @@ function Test-WindowsLinuxIntegration {
             
             Remove-Container -Session $Session -NameOrId $ClientID
             Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
-            Write-Host "======> Then there is correct response from Linux server  "
+            Write-Host "======> Then there is correct response from Linux server"
             if($Res -ne 0) {
                 throw "===> TCP test failed!"
             }
 
             Write-Host "===> PASSED: TcpLinuxWindowsConnectivity"
+        })
+    }
+
+    function Test-UdpLinuxWindowsConnectivity {
+        Param ([Parameter(Mandatory = $true)] [PSSessionT] $Session,
+               [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
+
+        $Job.StepQuiet($MyInvocation.MyCommand.Name, {
+            Write-Host "===> Running: UdpLinuxWindowsConnectivity"
+
+            Write-Host "======> Given controller for Windows-Linux integration"
+            $NetworkName = $TestConfiguration.DockerDriverConfiguration.TenantConfiguration.SingleSubnetNetwork.Name
+
+            Write-Host "======> Given running compute services"
+            Initialize-ComputeServices -Session $Session -TestConfiguration $TestConfiguration
+            Assert-IsAgentServiceEnabled -Session $Session
+            Start-Sleep -Seconds $WAIT_TIME_FOR_AGENT_INIT_IN_SECONDS
+
+            Write-Host "======> Given Windows container"
+            $ClientID = New-Container -Session $Session -NetworkName $NetworkName
+
+            Write-Host "======> When udp request is send to Linux server"
+            $Port = "9090"
+            $Message = "Lorem ipsum dolor sit amet, mundi zril epicuri nam no, eam et expetenda consulatu consequat."
+            $Command = (`
+                '$IpAddress = [System.Net.IPAddress]::Parse(\"{0}\");' +`
+                '$IpEndPoint = New-Object System.Net.IPEndPoint($IpAddress, {1});' +`
+                '$UdpClient = New-Object System.Net.Sockets.UdpClient(0);' +`
+                '$ReceiveTask = $UdpClient.ReceiveAsync();' +`
+                '$Data = [System.Text.Encoding]::UTF8.GetBytes(\"{2}\");' +`
+                'Foreach ($num in 1..10) {{$UdpClient.SendAsync($Data, $Data.length, $IpEndPoint) | Out-Null; Start-Sleep -Seconds 1}};' +`
+                '$GotMessage = $ReceiveTask.Wait(5000);' +`
+                'if($GotMessage) {{$ReceivedMessage = [System.Text.Encoding]::UTF8.GetString($ReceiveTask.Result.Buffer)}};' +`
+                'return $ReceivedMessage;'
+                ) -f $TestConfigurationWinLinux.LinuxVirtualMachineIp, $Port, $Message
+            $ReceivedMessage = Invoke-Command -Session $Session -ScriptBlock {
+                return & docker exec $Using:ClientID powershell -Command $Using:Command
+            }
+            Remove-Container -Session $Session -NameOrId $ClientID
+
+            Write-Host "======> Then there is correct response from Linux server"
+            if ($Message -ne $ReceivedMessage) {
+                throw "Received message: {0} is different from expected: {1}" -f $ReceivedMessage, $Message
+            }
+            Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
+            Write-Host "===> PASSED: UdpLinuxWindowsConnectivity"
         })
     }
 
@@ -87,6 +133,7 @@ function Test-WindowsLinuxIntegration {
         $TestConfigurationWinLinux.DockerDriverConfiguration.AuthUrl = $Env:DOCKER_DRIVER_AUTH_URL_LINUX_WINDOWS
 
         Test-IcmpLinuxWindowsConnectivity -Session $Session -TestConfiguration $TestConfigurationWinLinux
+        Test-UdpLinuxWindowsConnectivity -Session $Session -TestConfiguration $TestConfigurationWinLinux
         Test-TcpLinuxWindowsConnectivity -Session $Session -TestConfiguration $TestConfigurationWinLinux
     })
 }
