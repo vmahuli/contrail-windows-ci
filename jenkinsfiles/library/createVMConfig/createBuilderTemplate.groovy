abb929e17eb8f627fe997229e5a0b668599ffc33
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2017-12-21 12:50:34 +0100

    JW-1307: config generated by createBuilderTemplate script

---
+++ jenkinsfiles/library/createVMConfig/createBuilderTemplate.groovy
@@ -0,0 +1,31 @@
+def create(vc_hostname, vc_datacenter, vc_cluster, vc_folder, vc_network,
+           vc_username, vc_password, vm_template,
+           file_path = 'vm.vars') {
+  def vmConfig = """
+vcenter_hostname: ${vc_hostname}
+vcenter_user: ${vc_usr}
+vcenter_password: ${vc_psw}
+validate_certs: no
+datacenter_name: ${vc_datacenter}
+cluster_name: ${vc_cluster}
+vmware_folder: ${vc_folder}
+
+vm_template: ${vm_template}
+vm_role: builder
+
+vm_hardware:
+  memory_mb: 24576
+  num_cpus: 8
+
+vm_networks:
+  - name: ${vc_network}
+    type: dhcp
+
+vm_hdd:
+  - type: thin
+    size: 200
+"""
+  writeFile file: file_path, text: vmConfig
+}
+
+return this
