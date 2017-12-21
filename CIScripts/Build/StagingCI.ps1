26df893a094f9cc93a9ef7849cbede79b3251c32
Jacek Iżykowski <jacek.izykowski@codilime.com>
2017-12-21 10:45:43 +0100

    Temporary workaround for GitHub connectivity issues

--- CIScripts/Build/StagingCI.ps1
+++ CIScripts/Build/StagingCI.ps1
@@ -9,21 +9,40 @@ function Get-StagingRepos {
            [Parameter(Mandatory = $true)] [string] $VRouterBranch,
            [Parameter(Mandatory = $true)] [string] $ControllerBranch)
 
+    # $Repos = @{
+    #     "contrail-windows-docker" = [Repo]::new("https://github.com/codilime/contrail-windows-docker/",
+    #                                             $DriverBranch, "master", "src/github.com/codilime/contrail-windows-docker");
+    #     "contrail-windowsstubs" = [Repo]::new("https://github.com/codilime/contrail-windowsstubs/",
+    #                                           $WindowsstubsBranch, "windows", "windows/");
+    #     "contrail-build" = [Repo]::new("https://github.com/codilime/contrail-build",
+    #                                    $ToolsBranch, "windows", "tools/build/");
+    #     "contrail-sandesh" = [Repo]::new("https://github.com/codilime/contrail-sandesh",
+    #                                      $SandeshBranch, "windows", "tools/sandesh/");
+    #     "contrail-generateDS" = [Repo]::new("https://github.com/codilime/contrail-generateDS",
+    #                                         $GenerateDSBranch, "windows", "tools/generateDS/");
+    #     "contrail-vrouter" = [Repo]::new("https://github.com/codilime/contrail-vrouter",
+    #                                      $VRouterBranch, "windows", "vrouter/");
+    #     "contrail-controller" = [Repo]::new("https://github.com/codilime/contrail-controller",
+    #                                         $ControllerBranch, "windows3.1", "controller/")
+    # }
+
+    # Temporary workaround for GitHub connectivity issues.
     $Repos = @{
-        "contrail-windows-docker" = [Repo]::new("https://github.com/CodiLime/contrail-windows-docker/",
+        "contrail-windows-docker" = [Repo]::new("http://10.7.0.91/codilime/contrail-windows-docker.git",
                                                 $DriverBranch, "master", "src/github.com/codilime/contrail-windows-docker");
-        "contrail-windowsstubs" = [Repo]::new("https://github.com/CodiLime/contrail-windowsstubs/",
+        "contrail-windowsstubs" = [Repo]::new("http://10.7.0.91/codilime/contrail-windowsstubs.git",
                                               $WindowsstubsBranch, "windows", "windows/");
-        "contrail-build" = [Repo]::new("https://github.com/CodiLime/contrail-build",
+        "contrail-build" = [Repo]::new("http://10.7.0.91/codilime/contrail-build.git",
                                        $ToolsBranch, "windows", "tools/build/");
-        "contrail-sandesh" = [Repo]::new("https://github.com/CodiLime/contrail-sandesh",
+        "contrail-sandesh" = [Repo]::new("http://10.7.0.91/codilime/contrail-sandesh.git",
                                          $SandeshBranch, "windows", "tools/sandesh/");
-        "contrail-generateDS" = [Repo]::new("https://github.com/CodiLime/contrail-generateDS",
+        "contrail-generateDS" = [Repo]::new("http://10.7.0.91/codilime/contrail-generateDS.git",
                                             $GenerateDSBranch, "windows", "tools/generateDS/");
-        "contrail-vrouter" = [Repo]::new("https://github.com/CodiLime/contrail-vrouter",
+        "contrail-vrouter" = [Repo]::new("http://10.7.0.91/codilime/contrail-vrouter.git",
                                          $VRouterBranch, "windows", "vrouter/");
-        "contrail-controller" = [Repo]::new("https://github.com/CodiLime/contrail-controller",
+        "contrail-controller" = [Repo]::new("http://10.7.0.91/codilime/contrail-controller.git",
                                             $ControllerBranch, "windows3.1", "controller/")
     }
+    
     return $Repos
 }
