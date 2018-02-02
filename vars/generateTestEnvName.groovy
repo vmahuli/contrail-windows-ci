2d5e9b63ee5d2478001713d69a3d632812f856e1
Mateusz Jabłoński <mateusz.jablonski@codilime.com>
2018-02-02 12:50:02 +0100

    Generate unique testenv name (#62)

--- vars/generateTestEnvName.groovy
+++ vars/generateTestEnvName.groovy
@@ -1,7 +1,3 @@
 def call() {
-    if (env.ZUUL_UUID) {
-        return env.ZUUL_UUID.split('-')[0]
-    } else {
-        return env.ghprbActualCommit.take(8)
-    }
+    return UUID.randomUUID().toString().split('-')[-1]
 }
