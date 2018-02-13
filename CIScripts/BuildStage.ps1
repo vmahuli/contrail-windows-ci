833ad1301d7f941128a2f32d687b83a0466ad0c6
jacek-izykowski <jacek.izykowski@codilime.com>
2018-02-13 14:56:05 +0100

    Upload artifacts to S: (#89)

--- CIScripts/BuildStage.ps1
+++ CIScripts/BuildStage.ps1
@@ -1,5 +1,9 @@
 . $PSScriptRoot\Common\Job.ps1
 
+$OutputRootDirectory = "output"
+$Password = $Env:WINCIDEV_PSW | ConvertTo-SecureString -asPlainText -Force
+$Credentials = New-Object System.Management.Automation.PSCredential ($Env:WINCIDEV_USR, $Password)
+
 if ($Env:COMPONENTS_TO_BUILD -eq "None") {
     $Job = [Job]::new("Copying ready artifacts instead of building")
 
@@ -8,16 +12,25 @@ if ($Env:COMPONENTS_TO_BUILD -eq "None") {
         $ArtifactsPath = $Env:READY_ARTIFACTS_PATH
     }
 
-    $OutputRootDirectory = "output"
     $DiskName = [Guid]::newGuid().Guid
-    $Password = $Env:WINCIDEV_PSW | ConvertTo-SecureString -asPlainText -Force
-    $Credentials = New-Object System.Management.Automation.PSCredential ($Env:WINCIDEV_USR, $Password)
     New-PSDrive -Name $DiskName -PSProvider "FileSystem" -Root $ArtifactsPath -Credential $Credentials
     Copy-Item ("$DiskName" + ":\*") -Destination $OutputRootDirectory -Recurse -Container
 
     $Job.Done()
 } else {
     & $PSScriptRoot\Build.ps1
+}
+
+if (Test-Path Env:UPLOAD_ARTIFACTS) {
+    $ArtifactsPath = "\\$Env:SHARED_DRIVE_IP\SharedFiles\WindowsCI-UploadedArtifacts"
+    $Subdir = "$Env:JOB_NAME\$Env:BUILD_NUMBER"
+    $DiskName = [Guid]::newGuid().Guid
+    New-PSDrive -Name $DiskName -PSProvider "FileSystem" -Root $ArtifactsPath -Credential $Credentials
+    Push-Location
+    Set-Location ($Diskname + ":\")
+    New-Item -Name $Subdir -ItemType directory
+    Pop-Location
+    Copy-Item ($OutputRootDirectory + "\*") -Destination ("$DiskName" + ":\" + $Subdir) -Recurse -Container
 }
 
 exit 0
