d96196d45e273c4192b5e9dc84b63ba850687518
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-03-13 18:00:00 +0100

    Add test for single-testbed usecase

--- CIScripts/Testenv/Testenv.Tests.ps1
+++ CIScripts/Testenv/Testenv.Tests.ps1
@@ -1,32 +1,34 @@
 . $PSScriptRoot\Testenv.ps1
 
 Describe "Testenv" {
-    It "can read controller config from a .yaml file" {
-        $Controller = Read-ControllerConfig -Path $YamlPath
 
-        $Controller.OS_credentials.Address | Should Be "1.2.3.1"
-        $Controller.OS_credentials.Port | Should Be "5000"
-        $Controller.OS_credentials.Username | Should Be "AzureDiamond"
-        $Controller.OS_credentials.Password | Should Be "hunter2"
+    Context "Example config" {
+        It "can read controller config from a .yaml file" {
+            $Controller = Read-ControllerConfig -Path $YamlPath
 
-        $Controller.Rest_API.Address | Should Be "1.2.3.1"
-        $Controller.Rest_API.Port | Should Be "8082"
+            $Controller.OS_credentials.Address | Should Be "1.2.3.1"
+            $Controller.OS_credentials.Port | Should Be "5000"
+            $Controller.OS_credentials.Username | Should Be "AzureDiamond"
+            $Controller.OS_credentials.Password | Should Be "hunter2"
 
-        $Controller.Default_Project | Should Be "ci_tests"
-    }
+            $Controller.Rest_API.Address | Should Be "1.2.3.1"
+            $Controller.Rest_API.Port | Should Be "8082"
 
-    It "can read configuration of testbeds from .yaml file" {
-        $Testbeds = Read-TestbedsConfig -Path $YamlPath
-        $Testbeds[0].Address | Should Be "1.2.3.2"
-        $Testbeds[1].Address | Should Be "1.2.3.3"
-        $Testbeds[0].Username | Should Be "TBUsername"
-        $Testbeds[1].Username | Should Be "TBUsername"
-        $Testbeds[0].Password | Should Be "TBPassword"
-        $Testbeds[1].Password | Should Be "TBPassword"
-    }
+            $Controller.Default_Project | Should Be "ci_tests"
+        }
 
-    BeforeEach {
-        $Yaml = @"
+        It "can read configuration of testbeds from .yaml file" {
+            $Testbeds = Read-TestbedsConfig -Path $YamlPath
+            $Testbeds[0].Address | Should Be "1.2.3.2"
+            $Testbeds[1].Address | Should Be "1.2.3.3"
+            $Testbeds[0].Username | Should Be "TBUsername"
+            $Testbeds[1].Username | Should Be "TBUsername"
+            $Testbeds[0].Password | Should Be "TBPassword"
+            $Testbeds[1].Password | Should Be "TBPassword"
+        }
+
+        BeforeEach {
+            $Yaml = @"
 controller:
   os_credentials:
     username: AzureDiamond
@@ -50,7 +52,28 @@ testbeds:
     username: TBUsername
     password: TBPassword
 "@
-        $YamlPath = "TestDrive:\TestYaml.yaml"
-        $Yaml | Out-File $YamlPath
+            $YamlPath = "TestDrive:\TestYaml.yaml"
+            $Yaml | Out-File $YamlPath
+        }
+    }
+
+    Context "Single Testbed" {
+        BeforeEach {
+            $Yaml = @"
+testbeds:
+  - name: Testbed1
+    address: 1.2.3.2
+    username: TBUsername
+    password: TBPassword
+"@
+            $YamlPath = "TestDrive:\TestYaml.yaml"
+            $Yaml | Out-File $YamlPath
+        }
+
+        It "can read a config file with a single testbed" {
+            $Testbeds = Read-TestbedsConfig -Path $YamlPath
+
+            $Testbeds[0].Name | Should Be "Testbed1"
+        }
     }
 }
