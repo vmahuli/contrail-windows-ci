40a5140867693cd9d454c1ddc25b2f836d61ef9b
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-03-15 11:16:35 +0100

    wip

---
+++ CIScripts/Test/NUnitReportFixup/NUnitReportFixup.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "NUnitReportFixup" {
+    Context "test-suite tag flattening" {
+        It "removes all test-suite tags except direct parents of test-case tags" {
+
+        }
+    }
+    Context "test name cleanup" {
+        It "makes names of tests more readable" {
+
+        }
+    }
+}
