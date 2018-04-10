48f06cc1c59cedd8569eaf3e2265e4034cc193ad
Michał Krasnoborski <mkrdln@gmail.com>
2018-04-10 15:16:59 +0200

    Print a link to published logs in logs (#84)

--- vars/publishToLogServer.groovy
+++ vars/publishToLogServer.groovy
@@ -1,3 +1,10 @@
+def showUrl(Map logServer, String destDir) {
+    def httpRootLocation = '/var/www/logs/'
+    if (destDir.contains(httpRootLocation)) {
+        echo ('logs published to http://' + logServer.addr + destDir.replace(httpRootLocation, '/'))
+    }
+}
+
 def call(Map logServer, String srcDir, String destDir, boolean mayThrow = true) {
     script {
         if (fileExists(srcDir)) {
@@ -5,6 +12,7 @@ def call(Map logServer, String srcDir, String destDir, boolean mayThrow = true) 
             def remoteDir = authority + ":" + destDir
             shellCommand "ssh", [authority, "mkdir", "-p", destDir]
             shellCommand "rsync", ["-r", srcDir + "/", remoteDir]
+            showUrl(logServer, destDir)
         } else {
             def message = "publishToLogServer: Directory '${src}' does not exist."
             if (mayThrow) {
