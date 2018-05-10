00f9db38d680c770dac539be172eb3877cd61349
Michał Kostrzewa <kostrzewamc@gmail.com>
2018-05-10 17:31:13 +0200

    Send GitHub comment with link to logs from Jenkinsfile. (#138)

---
+++ vars/sendGithubComment.groovy
@@ -0,0 +1,12 @@
+def call(String text) {
+    def body="""{
+            "body": "${text}",
+            "commit_id": "${env.ghprbActualCommit}",
+            "path": "/",
+            "position": 0
+    }"""
+    def response = httpRequest authentication: "codijenkinsbot", httpMode: 'POST', requestBody: body,
+        url: "https://api.github.com/repos/${env.ghprbGhRepository}/issues/${env.ghprbPullId}/comments"
+    println("Status: " + response.status)
+    println("Content: " + response.content)
+}
