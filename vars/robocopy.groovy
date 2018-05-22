19fc7c6f35499b2f223dd4b08f7f910aa3dd3de7
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-05-22 16:38:26 +0200

    returnStatus from robocopy.

--- vars/robocopy.groovy
+++ vars/robocopy.groovy
@@ -1,5 +1,5 @@
 def call(String srcDir, String destDir, String filter) {
-    def status = powershell script: "robocopy ${srcDir} ${destDir} ${filter} /S"
+    def status = powershell returnStatus: true, script: "robocopy ${srcDir} ${destDir} ${filter} /S"
     if (status != 1) {
         // robocopy makes extensive use of exit codes. We're interested in checking
         // exit code 1: One of more files were copied successfully.
