25d425c92058c2c33348aebbf93e23749d64a341
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-05-30 17:09:53 +0200

    Fix.

--- vars/publishCurrentDirToLogServer.groovy
+++ vars/publishCurrentDirToLogServer.groovy
@@ -1,6 +1,7 @@
 def call(String authority, String destDir, boolean mayThrow = true) {
     script {
         def remoteDir = authority + ":" + destDir
+        def srcDir = "."
         shellCommand "ssh", [authority, "mkdir", "-p", destDir]
         shellCommand "rsync", ["-r", srcDir + "/", remoteDir]
     }
