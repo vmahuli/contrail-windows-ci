63c1928490e0385ee582b4cbfd657e5f8f351abc
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-06-06 11:45:26 +0200

    CR changes.

--- vars/publishCurrentDirToLogServer.groovy
+++ vars/publishCurrentDirToLogServer.groovy
@@ -1,8 +1,6 @@
-def call(String authority, String destDir, boolean mayThrow = true) {
-    script {
-        def remoteDir = authority + ":" + destDir
-        def srcDir = "."
-        shellCommand "ssh", [authority, "mkdir", "-p", destDir]
-        shellCommand "rsync", ["-r", srcDir + "/", remoteDir]
-    }
+def call(String authority, String destDir) {
+    def remoteDir = authority + ":" + destDir
+    def srcDir = "."
+    shellCommand "ssh", [authority, "mkdir", "-p", destDir]
+    shellCommand "rsync", ["-r", srcDir + "/", remoteDir]
 }
