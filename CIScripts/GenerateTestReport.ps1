2be9ee70cad22d5c4d9e5d0ca010cf636a8fd542
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-06-29 12:24:57 +0200

    Fix parameter for GenerateTestReports.ps1

--- CIScripts/GenerateTestReport.ps1
+++ CIScripts/GenerateTestReport.ps1
@@ -1,13 +1,13 @@
 Param(
-    [Parameter(Mandatory = $true)] [string] $XmlsDir,
+    [Parameter(Mandatory = $true)] [string] $RawNUnitPath,
     [Parameter(Mandatory = $true)] [string] $OutputDir
 )
 
 . $PSScriptRoot\Common\Init.ps1
 . $PSScriptRoot\Report\GenerateTestReport.ps1
 
-if ((Test-Path $XmlsDir) -and (Get-ChildItem $XmlsDir)) {
-    Convert-TestReportsToHtml -XmlReportsDir $XmlsDir -OutputDir $OutputDir
+if ((Test-Path $RawNUnitPath) -and (Get-ChildItem $RawNUnitPath)) {
+    Convert-TestReportsToHtml -RawNUnitPath $RawNUnitPath -OutputDir $OutputDir
 } else {
-    Write-Warning "No report generated, directory $XmlsDir doesn't exist or is empty"
+    Write-Warning "No report generated, directory $RawNUnitPath doesn't exist or is empty"
 }
